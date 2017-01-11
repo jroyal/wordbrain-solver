bee35d1094832709fb6db3e45bd374a59c9890ec
James Royal <jhroyal@us.ibm.com>
2017-01-11 14:28:59 -0600

    Add some basic comments

--- grid/grid.go
+++ grid/grid.go
@@ -7,6 +7,7 @@ import (
 	"github.com/jroyal/wordbrain-solver/dictionary"
 )
 
+// Grid is an object to hold the game state while being solved
 type Grid struct {
 	Grid    [][]string
 	Size    int
@@ -14,28 +15,20 @@ type Grid struct {
 	results []string
 }
 
+// NewGrid will create a new Grid object, and build the dictionary used to check for correct words
 func NewGrid() *Grid {
 	g := new(Grid)
 	dictionary.LoadDict()
 	return g
 }
 
+// AddRow will add a row to the Grid object
 func (g *Grid) AddRow(r []string) {
 	g.Grid = append(g.Grid, r)
 	g.Size = len(r)
 }
 
-func generateCleanWalkPath(size int) [][]bool {
-	walkedPath := make([][]bool, size)
-	for i := 0; i < size; i++ {
-		walkedPath[i] = make([]bool, size)
-		for j := 0; j < size; j++ {
-			walkedPath[i][j] = false
-		}
-	}
-	return walkedPath
-}
-
+// GetAllPossibleWords of a given length for the current grid
 func (g *Grid) GetAllPossibleWords(length int) []string {
 	fmt.Printf("Getting all words sized %d\n", length)
 	var current []string
@@ -48,6 +41,19 @@ func (g *Grid) GetAllPossibleWords(length int) []string {
 	return g.results
 }
 
+// generateCleanWalkPath creates a clean [][]bool array represention of the grid
+func generateCleanWalkPath(size int) [][]bool {
+	walkedPath := make([][]bool, size)
+	for i := 0; i < size; i++ {
+		walkedPath[i] = make([]bool, size)
+		for j := 0; j < size; j++ {
+			walkedPath[i][j] = false
+		}
+	}
+	return walkedPath
+}
+
+// validMove checks to see if the requested position is a valid position to move to
 func (g *Grid) validMove(x int, y int, walkedPath [][]bool) bool {
 	if x >= g.Size || y >= g.Size || x < 0 || y < 0 {
 		return false
@@ -60,6 +66,7 @@ func (g *Grid) validMove(x int, y int, walkedPath [][]bool) bool {
 	return true
 }
 
+// existingResult checks to see if the new solution already exists
 func existingResult(list []string, s string) bool {
 	for _, elem := range list {
 		if elem == s {
@@ -69,6 +76,7 @@ func existingResult(list []string, s string) bool {
 	return false
 }
 
+// recursiveWalk takes a grid, a requested length and solves the grid for all words of that length
 func recursiveWalk(g *Grid, length int, x int, y int, current []string, walkedPath [][]bool) {
 	// fmt.Printf("x:%d y:%d current: %v walkedPath: %v\n", x, y, current, walkedPath)
 	current = append(current, g.Grid[y][x])
